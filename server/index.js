d27589cb1c489bce9b3dac00b1a9369180c39fdc
Ben Jacobs <ben.jacobs@vanti.co.uk>
2017-10-22 19:50:29 +0100

    update node-samsung-remote package.

--- server/index.js
+++ server/index.js
@@ -150,5 +150,5 @@ app.get('/hdmi', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('listening on port 3000!');
 });
